34f6bcfbcd4cdf6dd9abfbcaa8313b729e780df9
jk <jklai@gate.sinica.edu.tw>
2024-02-07 13:21:49 +0800

    update all

---
+++ R/count2fpkm.R
@@ -0,0 +1,26 @@
+count2fpkm <- function(count_mat, gene_length, returnTPM = FALSE){
+    stopifnot(is.vector(gene_length))
+    stopifnot(inherits(names(gene_length), "character"))
+    stopifnot(identical(rownames(count_mat), names(gene_length)))
+    
+    norm_cmat <- t(t(count_mat) / (colSums(count_mat) / 1e6))
+    fpkm_mat <- norm_cmat / (gene_length / 1000)
+    
+    if (returnTPM){
+        fpkm_mat <- t(t(fpkm_mat) / colSums(fpkm_mat)) * 1e6
+    }
+    
+    return(fpkm_mat)
+}
+
+
+count2tpm <- function(count_mat, gene_length){
+    stopifnot(is.vector(gene_length))
+    stopifnot(inherits(names(gene_length), "character"))
+    stopifnot(identical(rownames(count_mat), names(gene_length)))
+    
+    norm_mat <- count_mat / (gene_length / 1000)
+    tpm_mat <- t(t(norm_mat) / (colSums(norm_mat) / 1e6))
+    
+    return(tpm_mat)
+}
---
+++ R/gene_interest.R
@@ -0,0 +1,47 @@
+PLT_genes <- c(
+    PLT1 = "AT3G20840",
+    PLT2 = "AT1G51190",
+    PLT3 = "AT5G10510",
+    PLT4 = "AT5G17430",
+    PLT5 = "AT5G57390",
+    PLT7 = "AT5G65510"
+)
+
+
+RGF_genes <- c(
+    RGF1 = "AT5G60810",  # GLV11
+    RGF2 = "AT1G13620",  # GLV5, ERF62
+    RGF3 = "AT2G04025",  # GLV7
+    RGF4 = "AT3G30350",  # GLV3
+    RGF5 = "AT5G51451",  # GLV10, CLEL7
+    RGF6 = "AT4G16515",  # GLV1, CLEL6
+    RGF7 = "AT3G02240",  # GLV4
+    RGF8 = "AT2G03830",  # GLV6, CLEL2,
+    RGF9 = "AT5G64770"   # GLV2, CLEL9
+)
+
+
+RGFR_genes <- c(
+    RGFR1 = "AT3G24240",
+    RGFR2 = "AT5G48940",
+    RGFR3 = "AT4G26540"
+)
+
+
+RITF_genes <- c(
+    RITF1 = "AT2G12646"
+)
+
+
+SOD_genes <- c(
+    CSD1 = "AT1G08830",
+    CSD2 = "AT2G28190",
+    CSD3 = "AT5G18100",
+    FSD1 = "AT4G25100",
+    FSD2 = "AT5G51100",
+    FSD3 = "AT5G23310",
+    MSD1 = "AT3G10920",
+    MSD2 = "AT3G56350",
+    NULL_01 = "AT4G00651",
+    NULL_02 = "AT4G15562"
+)
