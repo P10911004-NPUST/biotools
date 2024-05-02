b6f9e0304c13195711de357ab84968044c41aedb
jklai <jklai@gate.sinica.edu.tw>
2024-05-02 09:48:21 +0800

    add function: gff2gtf

--- R/gtf_related.R
+++ R/gtf_related.R
@@ -1,15 +1,23 @@
-read_gtf <- function(gtf_file){
+suppressMessages({
     if (!require(readr)) install.packages("readr")
     library(readr)
+    if (!require(stringr)) install.packages("stringr")
+    library(stringr)
+})
+
+
+read_gtf <- function(gtf_file){
     gtf <- read_tsv(gtf_file, comment = "#", col_names = FALSE, show_col_types = FALSE)
     colnames(gtf) <- c("seqname", "source", "feature", "start", "end", "score", "strand", "frame", "attributes")
     return(gtf)
 }
 
+
 write_gtf <- function(x, file){
     write_tsv(x, file, col_names = FALSE, quote = "none", escape = "none")
     
 }
+
 
 extract_attribute_feature <- function(x, feature_name = NULL){
     if (is.null(feature_name)){
@@ -29,3 +37,27 @@ extract_attribute_feature <- function(x, feature_name = NULL){
     
     return(feat)
 }
+
+
+gff2gtf <- function(gff_file){
+    
+    col_names <- c("seqname", "source", "feature", "start", "end", "score", "strand", "frame", "attributes")
+    
+    if (inherits(gff_file, "character")){
+        gff_file <- read_tsv(gff_file, comment = "#", col_names = FALSE, show_col_types = FALSE)
+    }
+    
+    if (identical(colnames(gff_file), col_names)){
+        colnames(gff_file) <- col_names
+    }
+    
+    gff <- gff_file %>% 
+        mutate(attributes = str_replace_all(attributes, "=", " \"")) %>% 
+        mutate(attributes = str_replace_all(attributes, ";", "\"; "))
+    
+    return(gff)
+}
+
+
+
+
