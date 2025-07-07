14464374770bb1316a2c32e786656c1ce17eea76
jklai <jklai@gate.sinica.edu.tw>
2025-07-07 10:24:32 +0800

    update

--- R/gtf_related.R
+++ R/gtf_related.R
@@ -4,10 +4,14 @@ suppressMessages({
     if (!require(dplyr)) install.packages("dplyr")
 })
 
+gtf_colnames <- c(
+    "seqname", "source", "feature", 
+    "start", "end", "score", 
+    "strand", "frame", "attributes"
+)
 
-gtf_colnames <- c("seqname", "source", "feature", "start", "end", "score", "strand", "frame", "attributes")
-
-read_gtf <- function(gtf_file){
+read_gtf <- function(gtf_file)
+{
     gtf <- readr::read_tsv(gtf_file, comment = "#", col_names = FALSE, show_col_types = FALSE)
     colnames(gtf) <- gtf_colnames
     gtf$start <- as.numeric(format(gtf$start, scientific = FALSE))
@@ -22,17 +26,19 @@ write_gtf <- function(x, file){
 }
 
 
-extract_attribute_feature <- function(x, feature_name = NULL){
-    if (is.null(feature_name)){
+extract_attribute_feature <- function(x, feature_name = NULL)
+{
+    if (is.null(feature_name))
         cat("\n'feature_name' should not be null\n")
-    }
     
-    if (is.vector(x)){
+    if (is.vector(x))
+    {
         feat <- gsub(paste0('.*', feature_name, ' "([^"]+)".*'), "\\1", x)
         feat[sapply(feat, function(x) grepl(";", x))] <- NA_character_
     }
     
-    if (is.data.frame(x)){
+    if (is.data.frame(x))
+    {
         feat <- x[ , tail(colnames(x), 1), drop = TRUE]
         feat <- gsub(paste0('.*', feature_name, '\\s"([^"]+)".*'), "\\1", feat)
         feat[sapply(feat, function(x) grepl(";", x))] <- NA_character_
@@ -42,7 +48,8 @@ extract_attribute_feature <- function(x, feature_name = NULL){
 }
 
 
-gff2gtf <- function(gff_file){
+gff2gtf <- function(gff_file)
+{
     
     if (inherits(gff_file, "character")){
         gff_file <- readr::read_tsv(gff_file, comment = "#", col_names = FALSE, show_col_types = FALSE)
